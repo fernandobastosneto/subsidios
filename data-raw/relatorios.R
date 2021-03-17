cacf309cad234e475f9d734040f45e5ac81024ec
fernandobastosneto <fernando.bastos.neto@gmail.com>
2021-03-17 13:24:15 -0300

    adicionando relatorios comerciomundo

--- data-raw/relatorios.R
+++ data-raw/relatorios.R
@@ -10,7 +10,6 @@ retirar_lista_paises <- c("Não Definido", "Provisão de Navios e Aeronaves", "B
 lista_paises <- unique(comerciobr::sh1_df$no_pais)
 lista_paises_filtrada <- lista_paises[! lista_paises %in% retirar_lista_paises]
 
-# purrr::walk(lista_paises_filtrada, barao::relatorio_brasil_pais)
 
 barao::relatorio_brasil_pais("China")
 
@@ -24,24 +23,34 @@ barao::relatorio_brasil_pais("China")
 # 
 # # Lendo a base de dados COMTRADE
 # 
+lista_paises_comtrade <- comerciomundo::dic_comtrade_mdic
+
 # exp_imp_sh2 <- map_df(here("input", "Comtrade", "arquivos_filtrados", paths_arquivos_filtrados), read_csv)
 # 
 # # Lista de países sem dados suficientes no Comtrade  
 # 
-# retirar_lista_paises_comtrade <- c("Areas, nes", "Chad", "Cuba", "Dem. People's Rep. of Korea", "Dem. Rep. of the Congo",
-#                                    "Djibouti", "Equatorial Guinea", "Eritrea", "Faeroe Isds", "Free Zones", "Gabon", 
-#                                    "Gibraltar", "Grenada", "Guinea-Bissau", 
-#                                    "Haiti", "Holy See (Vatican City State)", "LAIA, nes", "Liberia", "Libya", 
-#                                    "Marshall Isds", "Mayotte", "Nauru", "Neth. Antilles", "Oceania, nes", "Other Africa, nes",
-#                                    "Other Asia, nes", "Br. Antarctic Terr.", "Other Europe, nes", "Antarctica", 
-#                                    "North America and Central America, nes", "San Marino", "Sao Tome and Principe", "Somalia",
-#                                    "Tajikistan", "Turkmenistan", "Tuvalu", "Wallis and Futuna Isds", "Norfolk Isds",
-#                                    "Anguilla", "Br. Indian Ocean Terr.", "Cocos Isds", "Falkland Isds (Malvinas)", "Niue",
-#                                    "Saint Helena", "Tokelau", "Christmas Isds", "American Samoa", "Fr. South Antarctic Terr.",
-#                                    "Guam", "Saint Pierre and Miquelon", "United States Minor Outlying Islands", "South Georgia and the South Sandwich Islands",
-#                                    "N. Mariana Isd", "Heard Island and McDonald Islands", "Pitcairn", "Special Categories", "Bouvet Island",
-#                                    "Neutral Zone", "Saint Maarten", "N. Mariana Isds", "Western Sahara", "Bunkers", "Curaçao", "Serbia and Montenegro",
-#                                    "Bonaire", "South Sudan", "Syria", "Saint Barthélemy", "Br. Virgin Isds", "Cayman Isds")
+retirar_lista_paises_comtrade <- c("Areas, nes", "Chad", "Cuba", "Dem. People's Rep. of Korea", "Dem. Rep. of the Congo",
+                                   "Djibouti", "Equatorial Guinea", "Eritrea", "Faeroe Isds", "Free Zones", "Gabon",
+                                   "Gibraltar", "Grenada", "Guinea-Bissau",
+                                   "Haiti", "Holy See (Vatican City State)", "LAIA, nes", "Liberia", "Libya",
+                                   "Marshall Isds", "Mayotte", "Nauru", "Neth. Antilles", "Oceania, nes", "Other Africa, nes",
+                                   "Other Asia, nes", "Br. Antarctic Terr.", "Other Europe, nes", "Antarctica",
+                                   "North America and Central America, nes", "San Marino", "Sao Tome and Principe", "Somalia",
+                                   "Tajikistan", "Turkmenistan", "Tuvalu", "Wallis and Futuna Isds", "Norfolk Isds",
+                                   "Anguilla", "Br. Indian Ocean Terr.", "Cocos Isds", "Falkland Isds (Malvinas)", "Niue",
+                                   "Saint Helena", "Tokelau", "Christmas Isds", "American Samoa", "Fr. South Antarctic Terr.",
+                                   "Guam", "Saint Pierre and Miquelon", "United States Minor Outlying Islands", "South Georgia and the South Sandwich Islands",
+                                   "N. Mariana Isd", "Heard Island and McDonald Islands", "Pitcairn", "Special Categories", "Bouvet Island",
+                                   "Neutral Zone", "Saint Maarten", "N. Mariana Isds", "Western Sahara", "Bunkers", "Curaçao", "Serbia and Montenegro",
+                                   "Bonaire", "South Sudan", "Syria", "Saint Barthélemy", "Br. Virgin Isds", "Cayman Isds", "Fmr Panama-Canal-Zone",
+                                   "Czechoslovakia", "Bhutan", "Cook Isds", "Lao People's Dem. Rep.", "Dominica",
+                                   "Guadeloupe", "French Guiana", "Fmr Dem. Yemen", "Martinique", "FS Micronesia", "Papua New Guinea",
+                                   "Réunion", "Turks and Caicos Isds", "Vanuatu", "Venezuela", "US Virgin Isds")
 # 
-# lista_paises <- unique(exp_imp_sh2$NO_PAIS)
-# lista_paises <- lista_paises[! lista_paises %in% retirar_lista_paises_comtrade]
+lista_paises_comtrade_filtrada <- lista_paises_comtrade %>% 
+  dplyr::filter(! text %in% retirar_lista_paises_comtrade) %>% 
+  dplyr::filter(str_detect(text, ", nes$", negate = T)) %>% 
+  pull(text)
+
+# purrr::walk(lista_paises_comtrade_filtrada, barao::relatorio_pais_mundo)
+
